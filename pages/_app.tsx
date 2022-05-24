2b30173c71daa517739a3b12c49a992c1b1b4535
Steven <ssteven075@gmail.com>
2022-05-24 12:04:29 +0700

    add title dan description meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<title>Url Shortener</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta name="description" content="stevenn.tech is a platform where you can shorten any link for free."></meta>
+				<meta property="og:title" content="Home - stevenn.tech"></meta>
+				<meta
+					property="og:description"
+					content="stevenn.tech is a platform where you can shorten any link for free."
+				></meta>
 			</Head>
 			<SnackbarProvider
 				anchorOrigin={{
